42685907336b1b46b7e209aaecf31521741838db
Nguyễn Thái Hoàng <tindecken@gmail.com>
2017-08-25 17:30:53 +0700

    second commit

--- src/renderer/components/HomePage.vue
+++ src/renderer/components/HomePage.vue
@@ -1,22 +1,9 @@
 <template>
-  <div class="container">
-      <h1 class="title">
-        Hello World
-      </h1>
-      <p class="subtitle">
-        My first website with <strong>Bulma</strong>!
-      </p>
-      <a class="button is-white">White</a>
-    <a class="button is-light">Light</a>
-    <a class="button is-dark">Dark</a>
-    <a class="button is-black">Black</a>
-    <a class="button is-link">Link</a>
-    <a class="button is-primary">Primary</a>
-<a class="button is-info">Info</a>
-<a class="button is-success">Success</a>
-<a class="button is-warning">Warning</a>
-<a class="button is-danger">Danger</a>
-    </div>
+    <div class="container is-fullhd">
+  <div class="notification">
+    This container is <strong>fullwidth</strong> <em>until</em> the <code>$fullhd</code> breakpoint.
+  </div>
+</div>
 </template>
 
 <script>
