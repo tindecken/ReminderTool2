d24f54c9c2cc8718972dd31abda7dfb120ac00f2
Nguyễn Thái Hoàng <tindecken@gmail.com>
2017-09-29 21:36:02 +0700

    [U] for two environment not able to do same code above validation

--- src/renderer/components/HomePage.vue
+++ src/renderer/components/HomePage.vue
@@ -3,15 +3,13 @@
       <div class="columns is-gapless is-mobile">
           <div class="column is-three-quaters">
             <div class="control has-icons-right">
-              <input autofocus="true" v-model="number" v-on:keyup="isDisabled" v-validate.initial="number" data-vv-rules="required|between:1750,3600" class="input is-info is-large" type="text" placeholder="Input number between 1750-3600">
+              <!-- comment one in two <input> for validation in 2 environment -->
+              <!-- <input autofocus="true" v-model="number" v-on:keyup="isDisabled" v-validate.initial="number" data-vv-rules="required|between:1750,3600" class="input is-info is-large" type="text" placeholder="Input number between 1750-3600"> -->
+              <input autofocus="true" v-model="number" v-on:keyup="isDisabled" v-validate="'required|between:1750,3600'" class="input is-info is-large" type="text" placeholder="Input number between 1750-3600">
               <span class="icon is-right">
                 <i :class="{'fa fa-check': !disabled, 'fa fa-exclamation-triangle': disabled}"></i>
               </span>
             </div>
-              <!-- <input name="field"  v-model="number" v-validate.initial="number" data-vv-rules="'required|email'" class="input is-info" type="text" placeholder="Input number between 1750-3600">
-              <p class="text-danger" v-if="errors.has('email')">{{ errors.first('email') }}</p> -->
-              
-              <!-- <p class="text-danger" v-if="errors.has('number')">{{ errors.first('number') }}</p> -->
           </div>
           <div class="column is-narrow-mobile is-narrow-tablet is-narrow-desktop">
               <a class="button is-outlined is-info is-large" v-on:click="!disabled && go(number)" v-bind:disabled="disabled">GO !</a>
